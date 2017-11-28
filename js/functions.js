f97b0beafc3dc494b7c33960ded1c5ef250d845d
pvgrajalesg <pvgrajalesg@unal.edu.co>
2017-11-28 15:05:18 -0500

    Se agrega elementos html y estilos

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+(function(){
+    var img= document.querySelector('img');
+
+    var imagenes=["../img/imagen1.jpg", "../img/imagen2.jpg", "../img/imagen3.jpg", "../img/imagen4.jpg", "../img/imagen5.jpg", "../img/imagen6.jpg", "../img/imagen7.jpg"]
+    img.src=imagenes[3];
+}
+());
