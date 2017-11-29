09a346b327bd98079c13892684bc50f076ea9f55
pvgrajalesg <pvgrajalesg@unal.edu.co>
2017-11-28 21:16:16 -0500

    Se agrega evento para radios buttons

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,29 @@
 (function(){
-    var img= document.querySelector('img');
+    var radios= document.querySelectorAll('input');
+    var img= document.querySelectorAll('img');
+    console.log(radios[0]);
 
-    var imagenes=["../img/imagen1.jpg", "../img/imagen2.jpg", "../img/imagen3.jpg", "../img/imagen4.jpg", "../img/imagen5.jpg", "../img/imagen6.jpg", "../img/imagen7.jpg"]
-    img.src=imagenes[3];
+    	for (var i = 0, length = radios.length; i < length; i++)
+		{
+			radios[i].addEventListener("click", handle(radios[i].getAttribute("id")));
+		}
+
+		function handle(id){
+			//if(radios[i].checked){
+				console.log(id);
+    			if(id=="1"){
+    				img[0].style.opacity= 1;
+    			}else if(id=="2"){
+    				img[1].style.opacity= 1;
+    			}else if(id=="3"){
+    				img[2].style.opacity= 1;
+    			}
+			//}
+		}
+
+    //radios.addEventListener("click", handle);
+
+  /*  var imagenes=["../img/imagen1.jpg", "../img/imagen2.jpg", "../img/imagen3.jpg", "../img/imagen4.jpg", "../img/imagen5.jpg", "../img/imagen6.jpg", "../img/imagen7.jpg"]
+    img.src=imagenes[3];*/
 }
 ());
