f7148f17e25110a1ab0909e207b84932b13eb4dc
pvgrajalesg <pvgrajalesg@unal.edu.co>
2017-11-29 14:02:18 -0500

    Se implementa breakpoints

--- js/functions.js
+++ js/functions.js
@@ -5,9 +5,18 @@
 	var labelForward= document.querySelector('.gallery__control--forward');
 	img[0].style.opacity= 1;
 
+	//Cambio de imagen con el teclado
 	window.addEventListener('keydown', moveSelection);
+
+	//Cambio de imagen con flechas
 	labelBackward.addEventListener("click", moveLeft);
 	labelForward.addEventListener("click", moveRight);
+
+	//Cambio de imagen con radio button
+	radios[0].addEventListener("click", imageSelectionButton);
+	radios[1].addEventListener("click", imageSelectionButton);
+	radios[2].addEventListener("click", imageSelectionButton);
+	radios[3].addEventListener("click", imageSelectionButton);
 
 	function moveSelection(evt) {
 		switch (evt.keyCode) {
@@ -22,95 +31,42 @@
 
 	function moveLeft(){
 		if(radios[1].checked){
-			img[0].style.opacity= 1;
-			radios[1].checked=false;
-			radios[0].checked=true;
-			img[1].style.opacity= 0;
-			img[2].style.opacity= 0;
-			img[3].style.opacity= 0;
+			imageSelectionArrow(0);
 		}else if(radios[2].checked){
-			img[1].style.opacity= 1;
-			radios[2].checked=false;
-			radios[1].checked=true;
-			img[0].style.opacity= 0;
-			img[2].style.opacity= 0;
+			imageSelectionArrow(1);
 		}else if(radios[3].checked){
-			img[2].style.opacity= 1;
-			radios[3].checked=false;
-			radios[2].checked=true;
-			img[0].style.opacity= 0;
-			img[1].style.opacity= 0;
-			img[3].style.opacity= 0;
+			imageSelectionArrow(2);
 		}
 	}
 
 	function moveRight(){
 		if(radios[0].checked){
-			img[1].style.opacity= 1;
-			radios[0].checked=false;
-			radios[1].checked=true;
-			img[0].style.opacity= 0;
-			img[2].style.opacity= 0;
-			img[3].style.opacity= 0;
+			imageSelectionArrow(1);
 		}else if(radios[1].checked){
-			img[2].style.opacity= 1;
-			radios[1].checked=false;
-			radios[2].checked=true;
-			img[0].style.opacity= 0;
-			img[1].style.opacity= 0;
-			img[3].style.opacity= 0;
+			imageSelectionArrow(2);
 		}else if(radios[2].checked){
-			img[3].style.opacity= 1;
-			radios[2].checked=false;
-			radios[3].checked=true;
-			img[0].style.opacity= 0;
-			img[1].style.opacity= 0;
-			img[2].style.opacity= 0;
+			imageSelectionArrow(3);
 		}
 	}
 
-	radios[0].addEventListener("click", function(){
-		radios[1].checked=false;
-		radios[2].checked=false;
-		radios[3].checked=false;
-		radios[0].checked=true;
-		img[0].style.opacity= 1;
-		img[1].style.opacity= 0;
-		img[2].style.opacity= 0;
-		img[3].style.opacity= 0;
-	});
+	function imageSelectionArrow(id){
+		radios[id].checked=true;
+		for (let index = 0; index < img.length; index++) {
+			img[index].style.opacity=0;
+			
+		}
+		img[id].style.opacity=1;
+	}
 
-	radios[1].addEventListener("click", function(){
-		radios[0].checked=false;
-		radios[2].checked=false;
-		radios[3].checked=false;
-		radios[1].checked=true;
-		img[0].style.opacity= 0;
-		img[1].style.opacity= 1;
-		img[2].style.opacity= 0;
-		img[3].style.opacity= 0;
-	});
+	function imageSelectionButton(evt){
+		var id = event.currentTarget.getAttribute("id");
+		radios[id].checked=true;
+		for (let index = 0; index < img.length; index++) {
+			img[index].style.opacity=0;
+			
+		}
+		img[id].style.opacity=1;
+	}
 
-	radios[2].addEventListener("click", function(){
-		radios[0].checked=false;
-		radios[1].checked=false;
-		radios[3].checked=false;
-		radios[2].checked=true;
-		img[0].style.opacity= 0;
-		img[1].style.opacity= 0;
-		img[2].style.opacity= 1;
-		img[3].style.opacity= 0;
-	});
-
-	radios[3].addEventListener("click", function(){
-		radios[0].checked=false;
-		radios[1].checked=false;
-		radios[2].checked=false;
-		radios[3].checked=true;
-		img[0].style.opacity= 0;
-		img[1].style.opacity= 0;
-		img[2].style.opacity= 0;
-		img[3].style.opacity= 1;
-	});
 }
 ());
