7767b6ef01cda630347a9a40e07eec1e9d3ba411
pvgrajalesg <pvgrajalesg@unal.edu.co>
2017-11-29 11:54:15 -0500

    Se agrega el paso de las imagenes con el teclado y los radio buttons

--- js/functions.js
+++ js/functions.js
@@ -5,7 +5,22 @@
 	var labelForward= document.querySelector('.gallery__control--forward');
 	img[0].style.opacity= 1;
 
-	labelBackward.addEventListener("click", function(){
+	window.addEventListener('keydown', moveSelection);
+	labelBackward.addEventListener("click", moveLeft);
+	labelForward.addEventListener("click", moveRight);
+
+	function moveSelection(evt) {
+		switch (evt.keyCode) {
+			case 37:
+			moveLeft();
+			break;
+			case 39:
+			moveRight();
+			break;
+			}
+		};
+
+	function moveLeft(){
 		if(radios[1].checked){
 			img[0].style.opacity= 1;
 			radios[1].checked=false;
@@ -27,9 +42,9 @@
 			img[1].style.opacity= 0;
 			img[3].style.opacity= 0;
 		}
-	});
+	}
 
-	labelForward.addEventListener("click", function(){
+	function moveRight(){
 		if(radios[0].checked){
 			img[1].style.opacity= 1;
 			radios[0].checked=false;
@@ -52,6 +67,50 @@
 			img[1].style.opacity= 0;
 			img[2].style.opacity= 0;
 		}
+	}
+
+	radios[0].addEventListener("click", function(){
+		radios[1].checked=false;
+		radios[2].checked=false;
+		radios[3].checked=false;
+		radios[0].checked=true;
+		img[0].style.opacity= 1;
+		img[1].style.opacity= 0;
+		img[2].style.opacity= 0;
+		img[3].style.opacity= 0;
+	});
+
+	radios[1].addEventListener("click", function(){
+		radios[0].checked=false;
+		radios[2].checked=false;
+		radios[3].checked=false;
+		radios[1].checked=true;
+		img[0].style.opacity= 0;
+		img[1].style.opacity= 1;
+		img[2].style.opacity= 0;
+		img[3].style.opacity= 0;
+	});
+
+	radios[2].addEventListener("click", function(){
+		radios[0].checked=false;
+		radios[1].checked=false;
+		radios[3].checked=false;
+		radios[2].checked=true;
+		img[0].style.opacity= 0;
+		img[1].style.opacity= 0;
+		img[2].style.opacity= 1;
+		img[3].style.opacity= 0;
+	});
+
+	radios[3].addEventListener("click", function(){
+		radios[0].checked=false;
+		radios[1].checked=false;
+		radios[2].checked=false;
+		radios[3].checked=true;
+		img[0].style.opacity= 0;
+		img[1].style.opacity= 0;
+		img[2].style.opacity= 0;
+		img[3].style.opacity= 1;
 	});
 }
 ());
